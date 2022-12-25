9946aeb1192b518cca7e01bb6409c4e2ab5d8de9
JacobTonder <jacob@softwarekonsulenten.dk>
2022-12-25 22:48:11 +0100

    Cleanup

--- PCFDateTimeYearOnly/index.ts
+++ PCFDateTimeYearOnly/index.ts
@@ -57,7 +57,6 @@ export class PCFDateTimeYearOnly implements ComponentFramework.StandardControl<I
         this.renderView(context);
     }
 
-
     /**
      * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
      * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
